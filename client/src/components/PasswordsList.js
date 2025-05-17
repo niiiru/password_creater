e8c824d5f73a7c1c918b5f0982da3fd5d0c70b92
niiiru <nikaruu@icloud.com>
2025-05-18 01:40:20 +0200

    created passwordslist component, added it to the home page and modified it

---
+++ client/src/components/PasswordsList.js
@@ -0,0 +1,15 @@
+function PasswordsList() {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Website</th>
+          <th>Username</th>
+          <th>Password</th>
+          <th>Note</th>
+        </tr>
+      </table>
+    </div>
+  );
+}
+export default PasswordsList;
