4c4ab8a48eee78897d271fd199bee5a7522a140a
niiiru <nikaruu@icloud.com>
2025-06-15 18:11:45 +0200

    fixed registration

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,42 +1,54 @@
-import { useState } from "react";
+import { useRef } from "react";
+import { TextField, Button, Box } from "@mui/material";
 import axios from "axios";
 
 function Register() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
+  const emailInputRef = useRef(null);
+  const passwordInputRef = useRef(null);
   function register(e) {
     e.preventDefault();
-    axios.post("http://localhost:5001/api/v1/auth/register", {
-      email: username,
-      password,
-    });
+    axios
+      .post("http://localhost:5001/api/v1/auth/register", {
+        email: emailInputRef.current.value,
+        password: passwordInputRef.current.value,
+      })
+      .then(() => alert("Вы зарегистрировались!"))
+      .catch(({ response }) => {
+        alert(response.data.message);
+      });
   }
 
   return (
-    <div>
-      <form onSubmit={register}>
-        <div>
-          <label>Username</label>
-          <input
-            type="text"
-            required
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-          ></input>
-        </div>
-        <div>
-          <label>Password</label>
-          <input
-            type="password"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          ></input>
-        </div>
-        <button>Register</button>
-      </form>
-    </div>
+    <form onSubmit={register}>
+      <Box
+        sx={{
+          maxWidth: 300,
+          mx: "auto",
+          mt: 10,
+          display: "flex",
+          flexDirection: "column",
+          gap: 2,
+        }}
+      >
+        <TextField
+          label="Email"
+          type="email"
+          fullWidth
+          inputRef={emailInputRef}
+          required
+        />
+        <TextField
+          label="Пароль"
+          type="password"
+          fullWidth
+          inputRef={passwordInputRef}
+          required
+        />
+        <Button variant="contained" type="submit">
+          Зарегистрироваться
+        </Button>
+      </Box>
+    </form>
   );
 }
 
